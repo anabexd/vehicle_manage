387ab01c6980694cdcb886c12cc06a44d3316f13
anabexd <anabeatrizm438@gmail.com>
2024-07-01 19:47:51 -0300

    fix: dotenv

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,13 +3,23 @@ import { UserModule } from './user/user.module';
 import { VehicleModule } from './vehicle/vehicle.module';
 import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     UserModule,
     VehicleModule,
     AuthModule,
-    MongooseModule.forRoot(uri),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
   ],
   controllers: [],
   providers: [],
